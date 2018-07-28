2e4faf7cba93430a7f0a2316d850841c9367721c
AnthonyHadfield <anthonyhadfield60@yahoo.com>
2018-07-28 16:02:50 -0400

    Add files via upload

--- SR_dev_2_moveagent.py
+++ SR_dev_2_moveagent.py
@@ -3,136 +3,217 @@ import numpy as np
 import time
 
 class smooth_movement:
-
     def __init__(self):
         self.root = tk
-        self.frame = self.root.Canvas(bg='light green', height=270, width=368)
+        self.frame = self.root.Canvas(bg='sea green', height=380, width=588)
         self.frame.pack()
+
+    def grid(self):
+        """World"""
+        self.frame.create_rectangle(20, 20, 570, 350, fill='black')
+        """Specials"""
+        self.frame.create_rectangle(20, 20, 130, 130, fill='light green')
+        self.frame.create_rectangle(460, 20, 570, 130, fill='red')
+        self.frame.create_rectangle(20, 130, 240, 350, fill='grey')
+        self.frame.create_rectangle(350, 130, 570, 240, fill='grey')
+        """horizontal lines"""
+        self.frame.create_line(20, 20, 570, 20, fill='white', width=2)
+        self.frame.create_line(20, 130, 570, 130, fill='white', width=2)
+        self.frame.create_line(240, 240, 350, 240, fill='white', width=2)
+        self.frame.create_line(20, 350, 570, 350, fill='white', width=2)
+        """virtical lines"""
+        self.frame.create_line(20, 20, 20, 350, fill='white', width=2)
+        self.frame.create_line(130, 20, 130, 130, fill='white', width=2)
+        self.frame.create_line(240, 20, 240, 350, fill='white', width=2)
+        self.frame.create_line(350, 20, 350, 350, fill='white', width=2)
+        self.frame.create_line(460, 20, 460, 130, fill='white', width=2)
+        self.frame.create_line(460, 240, 460, 350, fill='white', width=2)
+        """Walls"""
+        """State 9"""
+        self.frame.create_line(460, 20, 573, 20, fill='white', width=5)
+        self.frame.create_line(570, 20, 570, 130, fill='white', width=5)
+        self.frame.create_line(460, 130, 570, 130, fill='white', width=5)
+        self.frame.create_line(570, 130, 570, 240, fill='white', width=5)
+        """State 8"""
+        self.frame.create_line(350, 20, 460, 20, fill='white', width=5)
+        self.frame.create_line(350, 130, 460, 130, fill='white', width=5)
+        """State 7"""
+        self.frame.create_line(18, 20, 130, 20, fill='white', width=5)
+        self.frame.create_line(20, 20, 20, 130, fill='white', width=5)
+        self.frame.create_line(20, 130, 130, 130, fill='white', width=5)
+        self.frame.create_line(20, 130, 20, 350, fill='white', width=5)
+        self.frame.create_line(18, 350, 130, 350, fill='white', width=5)
+        """State 6"""
+        self.frame.create_line(130, 20, 240, 20, fill='white', width=5)
+        self.frame.create_line(130, 130, 240, 130, fill='white', width=5)
+        """State 5"""
+        self.frame.create_line(240, 20, 350, 20, fill='white', width=5)
+        """State 4"""
+        self.frame.create_line(240, 130, 240, 240, fill='white', width=5)
+        self.frame.create_line(350, 130, 350, 240, fill='white', width=5)
+        """State 3"""
+        self.frame.create_line(240, 350, 350, 350, fill='white', width=5)
+        self.frame.create_line(240, 240, 240, 350, fill='white', width=5)
+        self.frame.create_line(130, 350, 240, 350, fill='white', width=5)
+        """State 2"""
+        self.frame.create_line(350, 350, 460, 350, fill='white', width=5)
+        self.frame.create_line(350, 240, 460, 240, fill='white', width=5)
+        """State 1"""
+        self.frame.create_line(570, 240, 570, 353, fill='white', width=5)
+        self.frame.create_line(460, 240, 570, 240, fill='white', width=5)
+        self.frame.create_line(460, 350, 570, 350, fill='white', width=5)
+
+        self.root.mainloop()
 
     def start_world(self):
         """World"""
-        self.frame.create_rectangle(20, 20, 350, 240, fill='black')
+        self.frame.create_rectangle(20, 20, 570, 350, fill='black')
         """horizontal lines"""
-        self.frame.create_line(20, 20, 350, 20, fill='white', width=2)
-        self.frame.create_line(20, 130, 350, 130, fill='white', width=2)
-        self.frame.create_line(20, 240, 350, 240, fill='white', width=2)
+        self.frame.create_line(20, 20, 570, 20, fill='white', width=2)
+        self.frame.create_line(20, 130, 570, 130, fill='white', width=2)
+        self.frame.create_line(20, 240, 570, 240, fill='white', width=2)
+        self.frame.create_line(20, 350, 570, 350, fill='white', width=2)
         """virtical lines"""
-        self.frame.create_line(20, 20, 20, 240, fill='white', width=2)
-        self.frame.create_line(130, 20, 130, 240, fill='white', width=2)
-        self.frame.create_line(240, 20, 240, 240, fill='white', width=2)
-        self.frame.create_line(350, 20, 350, 240, fill='white', width=2)
+        self.frame.create_line(20, 20, 20, 350, fill='white', width=2)
+        self.frame.create_line(130, 20, 130, 350, fill='white', width=2)
+        self.frame.create_line(240, 20, 240, 350, fill='white', width=2)
+        self.frame.create_line(350, 20, 350, 350, fill='white', width=2)
+        self.frame.create_line(460, 20, 460, 350, fill='white', width=2)
+        self.frame.create_line(570, 20, 570, 350, fill='white', width=2)
 
-        #self.root.mainloop()
 
-    def robot_state_1(self):
-        loop = 1
-        """agent"""
-        self.sensor_1 = self.frame.create_rectangle(275, 179, 284, 190, fill='red')
-        self.sensor_2 = self.frame.create_rectangle(289, 165, 300, 175, fill='red')
-        self.sensor_3 = self.frame.create_rectangle(315, 180, 306, 190, fill='red')
-        self.sensor_4 = self.frame.create_rectangle(291, 195, 300, 205, fill='red')
-        self.agent = self.frame.create_oval(280, 170, 310, 200, fill='cyan')
+    def robot(self):
+
+        print('current state =', current_state)
+        states = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        """call robot by state"""
+        if current_state == states[0]:
+            print('STATE =', states[0])
+            n = 0; m = 0; x = 0; y = 0; a = 10; b = 10
+
+        if states[1] == current_state:
+            n = -1; x = 110; m = 1; y = 0; a = 10; b = 10
+        """generic robot and sensor coordinates"""
+        r = (500 + n * x, 280 + m * y, 530 + n * x, 310 + m * y)
+        s1 = (495 + n * x, 290 + m * y, 504 + n * x, 300 + m * y)
+        s2 = (509 + n * x, 275 + m * y, 520 + n * x, 285 + m * y)
+        s3 = (535 + n * x, 290 + m * y, 526 + n * x, 300 + m * y)
+        s4 = (509 + n * x, 305 + m * y, 520 + n * x, 315 + m * y)
+
+        self.sensor_1 = self.frame.create_rectangle(s1, fill='red')
+        self.sensor_2 = self.frame.create_rectangle(s2, fill='red')
+        self.sensor_3 = self.frame.create_rectangle(s3, fill='red')
+        self.sensor_4 = self.frame.create_rectangle(s4, fill='red')
+        self.agent = self.frame.create_oval(r, fill='cyan')
         self.frame.update()
-        time.sleep(1)
+        time.sleep(0.5)
         """activate sensors"""
         for i in range(1, 6):
             time.sleep(0.5)
             if i == 1:
-                self.beam_1 = self.frame.create_polygon(266, 184, 274, 184, 274, 186, 266, 186, 266, 184, fill='yellow')
-                self.beam_2 = self.frame.create_polygon(294, 164, 294, 156, 296, 156, 296, 164, 294, 164, fill='yellow')
-                self.beam_3 = self.frame.create_polygon(316, 184, 324, 184, 324, 186, 316, 186, 316, 184, fill='yellow')
-                self.beam_4 = self.frame.create_polygon(295, 206, 295, 214, 297, 214, 297, 206, 294, 206, fill='yellow')
-                self.frame.update()
-            elif i == 2:
-                self.beam_5 = self.frame.create_polygon(256, 184, 264, 184, 264, 186, 256, 186, 256, 184, fill='yellow')
-                self.beam_6 = self.frame.create_polygon(294, 154, 294, 146, 296, 146, 296, 154, 294, 154, fill='yellow')
-                self.beam_7 = self.frame.create_polygon(326, 184, 334, 184, 334, 186, 326, 186, 326, 184, fill='yellow')
-                self.beam_8 = self.frame.create_polygon(295, 216, 295, 224, 297, 224, 297, 216, 294, 216, fill='yellow')
-                self.frame.update()
-            elif i == 3:
-                self.beam_9 = self.frame.create_polygon(246, 184, 254, 184, 254, 186, 246, 186, 246, 184, fill='yellow')
-                self.beam_10 = self.frame.create_polygon(294, 144, 294, 136, 296, 136, 296, 144, 294, 144,
-                                                         fill='yellow')
-                self.beam_11 = self.frame.create_polygon(336, 184, 344, 184, 344, 186, 336, 186, 336, 184,
-                                                         fill='yellow')
-                self.beam_12 = self.frame.create_polygon(295, 226, 295, 234, 297, 234, 297, 226, 294, 226, fill='yellow')
-                self.frame.update()
-            elif i == 4:
-                self.beam_13 = self.frame.create_polygon(236, 184, 244, 184, 244, 186, 236, 186, 236, 184,
-                                                         fill='yellow')
-                self.beam_14 = self.frame.create_polygon(294, 134, 294, 128, 296, 128, 296, 134, 294, 134, fill='red')
-                self.beam_15 = self.frame.create_polygon(346, 184, 352, 184, 352, 186, 346, 186, 346, 184, fill='red')
-                self.beam_16 = self.frame.create_polygon(295, 236, 295, 242, 297, 242, 297, 236, 294, 236, fill='red')
-                time.sleep(0.5)
-                """"add walls and block"""
-                self.frame.create_rectangle(241, 21, 349, 129, fill='grey')
-                self.frame.create_line(240, 130, 350, 130, fill='white', width=5)
-                self.frame.create_line(350, 129, 350, 242, fill='white', width=5)
-                self.frame.create_line(240, 240, 352, 240, fill='white', width=5)
-                self.frame.update()
-                """"delete sensors"""
-            elif i == 5:
-                time.sleep(0.5)
-                self.frame.delete(self.beam_1)
-                self.frame.delete(self.beam_2)
-                self.frame.delete(self.beam_3)
-                self.frame.delete(self.beam_4)
-                self.frame.delete(self.beam_5)
-                self.frame.delete(self.beam_6)
-                self.frame.delete(self.beam_7)
-                self.frame.delete(self.beam_8)
-                self.frame.delete(self.beam_9)
-                self.frame.delete(self.beam_10)
-                self.frame.delete(self.beam_11)
-                self.frame.delete(self.beam_12)
-                self.frame.delete(self.beam_13)
-                self.frame.delete(self.beam_14)
-                self.frame.delete(self.beam_15)
-                self.frame.delete(self.beam_16)
-                self.frame.update()
-
-    def smooth_transition(self):
-        coords = [[280, 170], [170, 170], [170, 60], [60, 60]]
-        self.start_world()
-        """initiate move agent and collect data"""
-        for i in range(1, 2):
-            if i == 1:
-                self.move = 'left'
-                self.robot_state_1()
-            s = self.frame.coords(self.agent)
-            previous_state_coords = [int(s[0]), int(s[1])]
-            for x in range(len(coords)):
-                if coords[x] == previous_state_coords:
-                    print('coords[x]=', x)
-                    previous_state = (len(coords)) - x
-            move = self.move
-            print('move=', move)
-            next_state = previous_state - previous_state +1
-            next_state_coords= coords[next_state]
-            start_sum = s[0]+s[3]
-            final_sum = start_sum -220
-        time.sleep(2)
-        """move agent"""
-        while start_sum > final_sum:
-            print('START SUM =', start_sum)
-            move_agent = np.array([-1, 0])
-            move_sensors = np.array([-1, 0])
-            self.frame.move(self.agent, move_agent[0], move_agent[1])
-            self.frame.move(self.sensor_1, move_sensors[0], move_agent[1])
-            self.frame.move(self.sensor_2, move_sensors[0], move_agent[1])
-            self.frame.move(self.sensor_3, move_sensors[0], move_agent[1])
-            self.frame.move(self.sensor_4, move_sensors[0], move_agent[1])
+                self.beam_1 = self.frame.create_polygon(486+n*x, 294+m*y, 494+n*x, 294+m*y, 494+n*x, 296+m*y, 486+n*x,
+                                                        296+m*y, 486+n*x, 294+m*y, fill='yellow')
+                self.beam_2 = self.frame.create_polygon(515+n*x, 274+m*y, 515+n*x, 266+m*y, 517+n*x, 266+m*y, 517+n*x,
+                                                        274+m*y, 514+n*x, 274+m*y, fill='yellow')
+                self.beam_3 = self.frame.create_polygon(536+n*x, 294+m*y, 544+n*x, 294+m*y, 544+n*x, 296+m*y, 536+n*x,
+                                                        296+m*y, 536+n*x, 294+m*y, fill='yellow')
+                self.beam_4 = self.frame.create_polygon(515+n*x, 316+m*y, 515+n*x, 324+m*y, 517+n*x, 324+m*y, 517+n*x,
+                                                        316+m*y, 514+n*x, 316+m*y, fill='yellow')
+            if i == 2:
+                self.beam_5 = self.frame.create_polygon(486-a+n*x, 294+m*y, 494-a+n*x, 294+m*y, 494-a+n*x, 296+m*y,
+                                                        486-a+n*x, 296+m*y, 486-a+n*x, 294+m*y, fill='yellow')
+                self.beam_6 = self.frame.create_polygon(515+n*x, 274-b+m*y, 515+n*x, 266-b+m*y, 517+n*x, 266-b+m*y,
+                                                        517+n*x, 274-b+m*y, 514+n*x, 274-b+m*y, fill='yellow')
+                self.beam_7 = self.frame.create_polygon(536+a+n*x, 294+m*y, 544+a+n*x, 294+m*y, 544+a+n*x, 296+m*y,
+                                                        536+a+n*x, 296+m*y, 536+a+n*x, 294+m*y, fill='yellow')
+                self.beam_8 = self.frame.create_polygon(515+n*x, 316+b+m*y, 515+n*x, 324+b+m*y, 517+n*x, 324+b+m*y,
+                                                        517+n*x, 316+b+m*y, 514+n*x, 316+b+m*y, fill='yellow')
+            if i == 3:
+                self.beam_9 = self.frame.create_polygon(486-2*a+n*x, 294+m*y, 494-2*a+n*x, 294+m*y, 494-2*a+n*x,
+                                                296+m*y, 486-2*a+n*x, 296+m*y, 486-2*a+n*x, 294+m*y, fill='yellow')
+                self.beam_10 = self.frame.create_polygon(515+n*x, 274-2*b+m*y, 515+n*x, 266-2*b+m*y, 517+n*x,
+                                                266-2*b+m*y, 517+n*x, 274-2*b+m*y, 514+n*x,274-2*b+m*y, fill='yellow')
+                self.beam_11 = self.frame.create_polygon(536+2*a+n*x, 294+m*y, 544+2*a+n*x, 294+m*y, 544+2*a+n*x,
+                                                296+m*y, 536+2*a+n*x, 296+m*y, 536+2*a+n*x, 294+m*y,fill='yellow')
+                self.beam_12 = self.frame.create_polygon(515+n*x, 316+2*b+m*y, 515+n*x, 324+2*b+m*y, 517+n*x,
+                                                324+2*b+m*y, 517+n*x, 316+2*b+m*y, 514+n*x, 316+2*b+m*y, fill='yellow')
+            if i == 4:
+                self.beam_13 = self.frame.create_polygon(486-3*a+n*x, 294+m*y, 494-3*a+n*x, 294+m*y, 494-3*a+n*x,
+                                                296+m*y, 486-3*a+n*x, 296+m*y, 486-3*a+n*x, 294+m*y, fill='yellow')
+                self.beam_14 = self.frame.create_polygon(515+n*x, 274-3*b+m*y, 515+n*x, 266-3*b+m*y, 517+n*x,
+                                                266-3*b+m*y, 517+n*x, 274-3*b+m*y, 514+n*x, 274-3*b+m*y, fill='yellow')
+                self.beam_15 = self.frame.create_polygon(536+3*a+n*x, 294+m*y, 544+3*a+n*x, 294+m*y, 544+3*a+n*x,
+                                                296+m*y, 536+3*a+n*x, 296+m*y, 536+3*a+n*x, 294+m*y, fill='yellow')
+                self.beam_16 = self.frame.create_polygon(515+n*x, 316+3*b+m*y, 515+n*x, 324+3*b+m*y, 517+n*x,
+                                                324+3*b+m*y, 517+n*x, 316+3*b+m*y, 514+n*x, 316+3*b+m*y, fill='yellow')
             self.frame.update()
+        """"add walls and block"""
+        if current_state == states[0]:
+            self.frame.create_rectangle(350, 130, 570, 240, fill='grey')
+            self.frame.create_line(570, 240, 570, 353, fill='white', width=5)
+            self.frame.create_line(460, 240, 570, 240, fill='white', width=5)
+            self.frame.create_line(460, 350, 570, 350, fill='white', width=5)
+        """"delete sensors"""
+        if i == 5:
+            self.frame.delete(self.beam_1)
+            self.frame.delete(self.beam_2)
+            self.frame.delete(self.beam_3)
+            self.frame.delete(self.beam_4)
+            self.frame.delete(self.beam_5)
+            self.frame.delete(self.beam_6)
+            self.frame.delete(self.beam_7)
+            self.frame.delete(self.beam_8)
+            self.frame.delete(self.beam_9)
+            self.frame.delete(self.beam_10)
+            self.frame.delete(self.beam_11)
+            self.frame.delete(self.beam_12)
+            self.frame.delete(self.beam_13)
+            self.frame.delete(self.beam_14)
+            self.frame.delete(self.beam_15)
+            self.frame.delete(self.beam_16)
+            self.frame.update()
+    def move_agent(self):
+        global current_state
+        """generic coordinates"""
+        coords = [[500, 280], [390, 280], [280, 280], [280, 170], [280, 60], [170, 60],
+                  [60, 60]], [390, 60], [500, 60]
+        self.start_world()
+        current_state = 1
+        """move the robot main LOOP"""
+        for i in range(1, 2):
+            self.robot()
             s = self.frame.coords(self.agent)
-            start_sum = start_sum -2
-            time.sleep(0.03)
+            print('s=', s)
+            current_coords = [int(s[0]), int(s[3])]
+            """generate a counting key"""
+            start_sum = s[0] + s[3]
+            print('start sum =', start_sum)
+            final_sum = start_sum - 220
+            print('final sum=', final_sum)
+            """moving the robot LOOP"""
+            while start_sum > final_sum:
+                move_agent = np.array([-1, 0])
+                move_sensors = np.array([-1, 0])
+                self.frame.move(self.agent, move_agent[0], move_agent[1])
+                self.frame.move(self.sensor_1, move_sensors[0], move_agent[1])
+                self.frame.move(self.sensor_2, move_sensors[0], move_agent[1])
+                self.frame.move(self.sensor_3, move_sensors[0], move_agent[1])
+                self.frame.move(self.sensor_4, move_sensors[0], move_agent[1])
+                self.frame.update()
+                s = self.frame.coords(self.agent)
+                print('coords = ', s)
+                """move robot one step at a time"""
+                start_sum = start_sum - 2
+                time.sleep(0.03)
 
         self.root.mainloop()
 
 
 data = smooth_movement()
 
-#data.__init__()
+# data.__init__()
+
+data.grid()
 
 # data.start_world()
 
@@ -140,4 +221,4 @@ data = smooth_movement()
 
 # data.agent_s1()
 
-data.smooth_transition()
+# data.move_agent()
